19418553059a5e7abc98026ab94d97926561ab23
JRossouwGitHub <51179785+JRossouwGitHub@users.noreply.github.com>
2020-12-05 01:05:36 +1300

    Updated Modules

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const subdomain = require('express-subdomain');
-const subdomainV = require('express-vhost');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 80;
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 const field = require(path.join(__dirname+'/public/field/field.js'));
 const office = require(path.join(__dirname+'/public/office/office.js'));
 
 app.use(subdomain('field', field));
 app.use(subdomain('office', office));
-
-subdomainV.register('field.valorantscrims.gg', field);
-subdomainV.register('office.valorantscrims.gg', office);
-
-app.use(subdomainV.vhost(app.enabled('trust proxy')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/public/index.html'));
@@ -39,4 +35,4 @@ app.get('*', (req, res, next) => {
     res.status(404).sendFile(path.join(__dirname+'/public/404.html'));
 });
 
-app.listen(port, () => console.log(`BMSBuddy listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
