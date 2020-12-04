b35efe6bc96e9a507c6c5f67757c07bb105d4541
JRossouwGitHub <51179785+JRossouwGitHub@users.noreply.github.com>
2020-12-04 23:51:52 +1300

    Changed subdomain method

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const subdomain = require('express-subdomain');
+const vhost = require('vhost');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 80;
 app.use(express.static(path.join(__dirname, 'public')));
 const field = require(path.join(__dirname+'/public/field/field.js'));
 const office = require(path.join(__dirname+'/public/office/office.js'));
+
+app.use(vhost('field.valorantscrims.gg', field));
+app.use(vhost('office.valorantscrims.gg', office));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/public/index.html'));
@@ -29,8 +33,5 @@ app.get('/create-new-account', (req, res) => {
 app.get('*', (req, res, next) => {
     res.status(404).sendFile(path.join(__dirname+'/public/404.html'));
 });
-
-app.use(subdomain('field', field));
-app.use(subdomain('office', office));
 
 app.listen(port, () => console.log(`BMSBuddy listening at http://localhost:${port}`));
